f4d5b83db3a9d80a35e14af98841fe6429f5a536
ynizon <ynizon@gmail.com>
2018-11-14 15:14:32 +0100

    Custom > Retrieve a report with only a url
    Feat

--- src/DareboostCustomAPI.php
+++ src/DareboostCustomAPI.php
@@ -13,7 +13,36 @@ namespace DareboostPHP;
  */
 class DareboostCustomAPI extends DareboostAPI
 {
-	
+	/**
+	Retrieve a report for an url
+	return boolean, or report array
+	*/	
+	public function getAnalysisReportForUrl($url){
+		$params = array("url"=>$url);
+		
+		$tab = array();
+		$analysis = $this->analysisLaunch($params);
+		if (isset($analysis["reportId"])){
+			$sReportId = $analysis["reportId"];
+			$params = array("reportId"=>$sReportId);
+			
+			$tab = array("status"=>202);//202 = In progress
+			$iRetry = 0;			
+			do{
+				sleep(15);
+				$tab = $this->getAnalysisReport($params);
+				echo var_dump($tab);
+				$iRetry++;
+				if ($tab["status"] == 200){
+					return $tab["report"];
+					exit();
+				}
+				
+			}while ($iRetry < 5 and $tab["status"] != 200);
+		}
+		return false;
+		
+	}
 }
 
 ?>
